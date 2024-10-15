bb9f08c7c599c3f178c2212d0c96581bb3646a1a
Zhiqin-Wei <zhiqin.daly.wei@mths.ca>
2024-10-15 11:15:50 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Daly
+ * Created on: oct 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber: number = 0
+
+randomNumber = -1
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+    randomNumber = randint(0, 1)
+    basic.clearScreen()
+
+    //if randomNumber == 0
+    if (randomNumber == 0) {
+    basic.showString("N")
+    }
+
+    //if randomNumber == 1
+    if (randomNumber == 1) {
+        basic.showString("Y")
+    }
+
+    // pause and show you are ready again
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+})
